cf54dbdb40a49e06caf2fc28a2ad96b31715bac9
Jessica Glomb <j.glomb@douglas.de>
2023-11-21 09:01:29 +0100

    Methoden, Methoden, Methoden.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿//using System.Text.Json;
-using System.Runtime.Serialization.Formatters.Binary;
-
-//using System.Text.Json;
+﻿using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ProductManagement
 
@@ -14,84 +11,99 @@ namespace ProductManagement
         public static void Main(string[] args)
         {
             while (true)
-            {
-                Console.WriteLine("Hey! Welcome to our Douglas Prodcut Management Site.");
-                Console.WriteLine("1: Add a product");
-                Console.WriteLine("2: Show a product");
-                Console.WriteLine("3: Edit a product");
-                Console.WriteLine("4: Delete a product");
-                Console.WriteLine("5: Close the Product Manager");
-                Console.WriteLine("6: Save Changes");
-                Console.WriteLine("7: Load Products");
-                Console.WriteLine("8: Save Changes in JSON");
-                Console.WriteLine("9: Load Products from JSON");
-
-                Console.WriteLine("Choose an option: ");
-
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        Add();
-                        break;
-                    case "2":
-                        Show();
-                        break;
-                    case "3":
-                        Edit();
-                        break;
-                    case "4":
-                        Delete();
-                        break;
-                    case "5":
-                        Close();
-                        break;
-                    case "6":
-                        Save();
-                        break;
-                    case "7":
-                        Load();
-                        break;
-                    case "8":
-                        JSONSave();
-                        break;
-                    case "9":
-                        JSONLoad();
-                        break;
-                    default:
-                        Console.WriteLine("Sorry, wrong input");
-                        break;
-                }
-
-                Console.WriteLine("\n");
-            }
+                CasesForUserChoice();
         }
 
+        private static void CasesForUserChoice()
+        {
+            string choice = DisplayUserOptionsGetsChoice();
+
+            switch (choice)
+            {
+                case "1":
+                    Add();
+                    break;
+                case "2":
+                    Show();
+                    break;
+                case "3":
+                    Edit();
+                    break;
+                case "4":
+                    Delete();
+                    break;
+                case "5":
+                    Save();
+                    break;
+                case "6":
+                    Load();
+                    break;
+                case "7":
+                    JSONSave();
+                    break;
+                case "8":
+                    JSONLoad();
+                    break;
+                case "9":
+                    Close();
+                    break;
+                default:
+                    Console.WriteLine("Sorry, wrong input");
+                    break;
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        private static string DisplayUserOptionsGetsChoice()
+        {
+            Console.WriteLine("Hey! Welcome to our Douglas Prodcut Management Site.");
+            Console.WriteLine("1: Add a product");
+            Console.WriteLine("2: Show a product");
+            Console.WriteLine("3: Edit a product");
+            Console.WriteLine("4: Delete a product");
+            Console.WriteLine("5: Close the Product Manager");
+            Console.WriteLine("6: Save Changes");
+            Console.WriteLine("7: Load Products");
+            Console.WriteLine("8: Save Changes in JSON");
+            Console.WriteLine("9: Load Products from JSON");
+
+            Console.WriteLine("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            return choice;
+        }
 
         public static void Add()
         {
-            Console.WriteLine("Enter L for Lipstick or P for Perfume: ");
+            GetProductInformation();
+        }
+
+        private static void GetProductInformation()
+        {
             string userChoiceLipstick = "L";
             string userChoicePerfume = "P";
+
+
+            var lipstickproduct = new Lipstick();
+            var perfumeproduct = new Perfume();
+
+            Console.WriteLine("Enter L for Lipstick or P for Perfume: ");
+
             string userChoice = Console.ReadLine();
 
             if (userChoice == userChoiceLipstick)
 
             {
-                var lipstickproduct = new Lipstick();
                 lipstickproduct.questionsAdd();
-
                 lipstickproduct.WriteToConsole();
                 products.Add(lipstickproduct);
 
             }
             else if (userChoice == userChoicePerfume)
             {
-                var perfumeproduct = new Perfume();
                 perfumeproduct.questionsAdd();
                 perfumeproduct.WriteToConsole();
-
                 products.Add(perfumeproduct);
             }
 
@@ -103,6 +115,11 @@ namespace ProductManagement
 
         public static void Show()
         {
+            DisplayEachProduct();
+        }
+
+        private static void DisplayEachProduct()
+        {
             foreach (var product in products)
             {
                 product.WriteToConsole();
@@ -110,6 +127,11 @@ namespace ProductManagement
         }
 
         public static void Edit()
+        {
+            ChooseProductToEdit();
+        }
+
+        private static void ChooseProductToEdit()
         {
             Show();
 
@@ -131,17 +153,16 @@ namespace ProductManagement
 
             else
             {
-                /*
-                 * TODO
-                 * Hier werden nur die Properties von der Mutterklasse Product erfasst.
-                 * Wie könnte man hier die spezifischen Properties ebenfalls erfassen?
-                 * Tipp: Typprüfung
-                 */
                 Console.WriteLine("Invalid Input");
             }
         }
 
         public static void Delete()
+        {
+            ChooseProductToDelete();
+        }
+
+        private static void ChooseProductToDelete()
         {
             Show();
 
@@ -160,7 +181,14 @@ namespace ProductManagement
 
             Console.WriteLine("The product has been successfully deleted\n");
         }
+
         public static void Save()
+        {
+            BinarySavingToLocalFile();
+
+        }
+
+        private static void BinarySavingToLocalFile()
         {
 #pragma warning disable SYSLIB0011
 
@@ -168,11 +196,14 @@ namespace ProductManagement
             BinaryFormatter formatter = new BinaryFormatter();
             formatter.Serialize(stream, products);
             stream.Close();
-
-
-
         }
+
         public static void Load()
+        {
+            BinaryLoadingFromLocalFile();
+        }
+
+        private static void BinaryLoadingFromLocalFile()
         {
             FileStream filestream = File.OpenRead(@"C:\Users\j.glomb\Documents\ProductFiles\productobjects.txt");
             BinaryFormatter formatter = new BinaryFormatter();
@@ -180,124 +211,35 @@ namespace ProductManagement
             filestream.Close();
             Show();
         }
+
         static void Close()
+        {
+            EndsProgram();
+        }
+
+        private static void EndsProgram()
         {
             Environment.Exit(0);
         }
 
         public static void JSONSave()
         {
-            {
-                //JsonSerializer serializer = new JsonSerializer();
-                //string json = JsonConvert.SerializeObject(products);
-                //using (StreamWriter file = File.CreateText("newy.txt"))
-                //{
-                //    JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
-                //    string Serialized = JsonConvert.SerializeObject(products, settings);
-                //    Console.WriteLine(Serialized);
-
-                //    //var properties = new JsonSerializerOptions {IncludeFields = true, WriteIndented = true };
-                //    //var jsonString = JsonSerializer.Serialize(products, properties);
-                //    //Console.WriteLine(jsonString);
-                //    //FileStream createStream = File.Create(path: "jsonproductobjects.json");
-                //    //        JsonSerializer serializer = new JsonSerializer();
-                //    //JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All};
-                //    //string Serialized = JsonConvert.SerializeObject(products, settings);    
-                //    //Console.WriteLine(Serialized);
-
-
-                //}
-
-
-
-                //JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
-                File.WriteAllText("newy.json", System.Text.Json.JsonSerializer.Serialize(products));
-
-
-                //static void JSONLoad()
-                //{
-                //    var properties = new JsonSerializerOptions { IncludeFields = true };
-                //    string jsonString = File.ReadAllText(@"C:\Users\j.glomb\Documents\ProductFiles\jsonproductobjects.txt");
-                //    List<Product>products = JsonSerializer.Deserialize<List<Product>>(jsonString, properties);
-                //    //JsonConverter<List<Product> vllt converten?;
-                //    Show();
-
-
-
-
-                //static void JSONSave()
-                //{
-                //    var properties = new JsonSerializerOptions { IncludeFields = true };
-                //    var jsonString = JsonSerializer.Serialize(products, properties);
-                //    File.WriteAllText("jsonproductobjects.json", jsonString);
-
-
-
-                //}
-                //static void JSONLoad()
-                //{
-                //    var properties = new JsonSerializerOptions { IncludeFields = true };
-                //    string jsonString = File.ReadAllText(@"C:\Users\j.glomb\Documents\ProductFiles\jsonproductobjects.txt");
-                //    List<Product> products = JsonSerializer.Deserialize<List<Product>>(jsonString, properties);
-                //    string jsonString = File.ReadAllText("jsonproductobjects.json");
-                //    products = JsonSerializer.Deserialize<List<Product>>(jsonString, properties);
-                //    //JsonConverter<List<Product> vllt converten?;
-                //    Show();
-                // //}
-                //public static void JSONLoad()
-                // {
-                // FileStream createStream = File.Create(path: "jsonproductobjects.json");
-                // JsonSerializer serializer = new JsonSerializer();
-                // JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
-                // string Serialized = JsonConvert.SerializeObject(products, settings);
-                // Console.WriteLine(Serialized);
-                // List<Product> deserializedList = JsonConvert.DeserializeObject<List<Product>>(Serialized, settings);
-
-                //var properties = new JsonSerializerOptions { IncludeFields = true,
-                //    WriteIndented = true
-                //    };
-
-                //string jsonString = File.ReadAllText(path: "jsonproductobjects.json");
-                //products = JsonSerializer.Deserialize<List<Product>>(jsonString, properties);
-                ////JsonConverter<List<Product> vllt converten?;
-                //Show();
-
-                //P value = JsonSerializer.Deserialize<List<Product>>(jsonString);
-                //Console.WriteLine(value is Product);
-            }
-
-            //}
-
+            JsonSavingToLocalFile();
 
         }
+
+        private static void JsonSavingToLocalFile()
+        {
+            File.WriteAllText("newy.json", System.Text.Json.JsonSerializer.Serialize(products));
+        }
+
         public static void JSONLoad()
         {
-            //JsonDeserializer deserializer = new JsonDeserializer();
-            //string json = JsonConvert.DeserializeObject(products);
-            //using (StreamWriter file = File.CreateText(@"C:\Users\j.glomb\Documents\ProductFiles\newy.txt"))
-            //{
-            //    JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
-            //    string Serialized = JsonConvert.SerializeObject(products, settings);
-            //    Console.WriteLine(Serialized);
+            JsonLoadingFromLocalFile();
+        }
 
-            //    //var properties = new JsonSerializerOptions {IncludeFields = true, WriteIndented = true };
-            //    //var jsonString = JsonSerializer.Serialize(products, properties);
-            //    //Console.WriteLine(jsonString);
-            //    //FileStream createStream = File.Create(path: "jsonproductobjects.json");
-            //    //        JsonSerializer serializer = new JsonSerializer();
-            //    //JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All};
-            //    //string Serialized = JsonConvert.SerializeObject(products, settings);    
-            //    //Console.WriteLine(Serialized);
-
-
-            //}
-            //File.WriteAllText(@"C:\Users\j.glomb\Documents\ProductFiles\newy.txt", JsonConvert.SerializeObject(products));
-            //var res = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText(@"C:\Users\j.glomb\Documents\ProductFiles\newy.txt"));
-            //string json = JsonConvert.DeserializeObject(File.ReadAllText(@"C:\Users\j.glomb\Documents\ProductFiles\newy.txt"));
-            //Console.WriteLine(res);
-
-            //JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
-
+        private static void JsonLoadingFromLocalFile()
+        {
             var products2 = System.Text.Json.JsonSerializer.Deserialize<List<Product>>(File.ReadAllText("newy.json"));
             foreach (var item in products2)
             {
@@ -306,6 +248,5 @@ namespace ProductManagement
 
             products = products2;
         }
-
     }
 }
